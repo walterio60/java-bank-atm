94a3b216d8c3c74e7cb60ad6b3a6cac1f728e24a
walterio60 <walterio1960@yahoo.com.ar>
2024-10-21 17:44:56 -0300

    Cambiamos la leyenda  JavaBank ATM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, JavaBank ATM!");
+        System.out.println("Hello, JavaBank ATM! control with Git.");
     }
 }
